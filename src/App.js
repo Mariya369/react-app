ae750d22730f5b00b2ddac84c999514ffe8d35ea
Mariya369 <mariyadotkova555@gmail.com>
2023-06-01 13:20:44 +0200

    Styling

--- src/App.js
+++ src/App.js
@@ -12,7 +12,8 @@ function App() {
         </h1>
         <Weather defaultCity = "Marbella" />
     <footer>
-This project is open-sourced on <a href="https://github.com/Mariya369/weather-app">GitHub</a> and was coded by Mariya Dotkova.
+This project was coded by <a href="https://www.mariyadotkova.com/" target="_blank" rel="noopener noreferrer">
+  Mariya Dotkova</a> , is open-sourced on <a href="https://github.com/Mariya369/weather-app" target="_blank" rel="noopener noreferrer">GitHub</a>, and is <a href="https://tourmaline-profiterole-67cde6.netlify.app/" target="_blank" rel="noopener noreferrer">hosted on Netlify</a>
     </footer>
     </div>
     </div>
--- src/WeatherTemperature.js
+++ src/WeatherTemperature.js
@@ -1,41 +1,13 @@
-import React, { useState } from "react";
+import React from "react";
 
 import "./WeatherTemperature";
  
 export default function WeatherTemperature(props) {
-const [unit, setUnit] = useState("celsius");
-function showFahrenheit(event) {
-    event.preventDefault();
-    setUnit("fahrenheit");
-}
-
-function showCelsius(event) {
-    event.preventDefault();
-    setUnit("celsius");
-}
-
-function fahrenheit() {
-   return (props.celsius * 9 / 5)+ 32;
-}
-
-if (unit === "celsius") {
-
-return (
-    <div className="WeatherTemperature" >
-    <span className="temperature">
-        {Math.round(props.celsius)}</span>
-        <span className="unit">
-            ºC I <a href="/" onClick={showFahrenheit}>ºF</a> </span>
-</div>
-);
-} else {
     return (
-    <div className="WeatherTemperature" >
-    <span className="temperature">
-        {Math.round(fahrenheit())}</span>
-        <span className="unit">
-           <a href="/" onClick={showCelsius}>ºC</a>{" "} I ºF</span>
-</div>
+        <div className="WeatherTemperature" >
+        <span className="temperature">
+            {Math.round(props.celsius)}</span>
+            <span className="unit">ºC</span>
+    </div>
     );
-}
-}
+    } 
